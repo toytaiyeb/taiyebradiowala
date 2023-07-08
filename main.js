8f1f0d003a593dfd97e674cd03383014b013e664
Taiyeb Radiowala <toy24052003@gmail.com>
2023-07-08 12:25:50 +0530

    day 4 started with changing the font , zindex, overall worded on making it look pretty

--- main.js
+++ main.js
@@ -84,9 +84,9 @@ if (objToRender === "rcomputerm") {
 function animate() {
   requestAnimationFrame(animate);
   //Here we could add some code to update the scene, adding some automatic movement
-  // object.rotation.x += 0.01;
-  // object.rotation.y += 0.01;
-  // object.rotation.z += 0.01;
+  object.rotation.x += 0.01;
+  object.rotation.y += 0.01;
+  object.rotation.z += 0.01;
   //Make the computer move
   if (object && objToRender === "rcomputerm") {
     //I've played with the constants here until it looked good 
--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-    
+let crsr=document.querySelector("#cursor")
+let crsr_b=document.querySelector("#cursor-b")
+
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left=dets.x+"px"
+    crsr.style.top=dets.y+"px"
+    crsr_b.style.left=dets.x-100+"px"
+    crsr_b.style.top=dets.y-100+"px"
+
+})
+
+
+
 function revelToSpan(){ 
     document.querySelectorAll(".reveal")
     .forEach(function(elem){
@@ -22,7 +34,7 @@ function loaderAnimation(){
     var tl=gsap.timeline();
 
 tl
-    .from(".child span",{
+    .from("#loader .child span",{
         y:"100%",
         duration:1,
         delay:.3,
@@ -30,7 +42,7 @@ tl
         ease:Power3.easeInOut,
     })
    
-    .to(".parent .child",{
+    .to("#loader .parent .child",{
         y:"-100%",
         duration:1,
         delay:.6,
@@ -61,12 +73,53 @@ tl
 }
 
 function svgAnimation(){
-    document.querySelector("#Visual>g").forEach(function(e){
-        let lines=e.childNodes[1].childNodes[1]
-        lines.style.strokeDasharray=lines.getTotalLenght()+'px'
-        lines.style.strokeDashoffset=lines.getTotalLenght()+'px'
+    document.querySelectorAll("#Visual>g").forEach(function(e){
+        let lines=e.childNodes[0].childNodes[0]
+        lines.style.strokeDasharray=lines.getTotalLength()+'px'
+        lines.style.strokeDashoffset=lines.getTotalLength()+'px'
+        
+        gsap.to("#Visual>g>g>path, #Visual>g>g>polyline",{
+            strokeDashoffset:0,
+            duration:3.5,
+            ease:Expo.Circ,
+            delay:4
+        })
+    
+    
+    
     })
 }
+// function changeWords(){
+//     let word=document.getElementById("mainwords2")
+    
+//     for(var i=0;i<arr.length;i++){
+
+//         word.innerHTML=""
+//         word.innerHTML=arr[i]
+
+//     }
+// }
+
+let arr=["Developer","Designer","Builder","Dreamer",]
+
+function textSequence(i) {
+
+    if (arr.length > i) {
+        setTimeout(function() {
+            document.getElementById("mainwords2").innerHTML = arr[i];
+            textSequence(++i);
+        }, 1000); // 1 second (in milliseconds)
+
+    } else if (arr.length == i) { // Loop
+        textSequence(0);
+        
+    }
+
+}
+
+
 revelToSpan()
 loaderAnimation()
 svgAnimation()
+// changeWords()
+textSequence(0)
